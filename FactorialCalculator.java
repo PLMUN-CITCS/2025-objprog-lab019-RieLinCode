d608e36b8cd1d22a1a6acd174cf41faca05070ed
Riel Joson <josonriel_bscs@plmun.edu.ph>
2025-03-12 13:40:43 +0800

    Submitting OBJPROG Week 05 - Laboratory # 19

---
+++ FactorialCalculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    public static int getNonNegativeInteger() {
+        Scanner userInput = new Scanner(System.in);
+        int number;
+
+        while (true) {
+            System.out.print("Enter a non-negative integer: ");
+
+            if (userInput.hasNextInt()) {
+                number = userInput.nextInt();
+
+                if (number >= 0) {
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter a non-negative integer: ");
+                }
+            } else {
+                System.out.println("Error: Invalid input. please enter a non-negative integer: ");
+                userInput.next();
+            }
+        }
+        return number;
+    
+    }
+
+    public static long calculateFactorial(int n) {
+        long factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        int number = getNonNegativeInteger();
+        long factorial = calculateFactorial(number);
+
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+}
+    
